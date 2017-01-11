ba555619c9aabdf6c2acbb9e1e47db3ddd53121a
mibzman <sb205>
2017-01-11 13:28:53 -0500

    Fixed Incorrect ID On Generated Fragment Layout

--- androidTestProject/androidTestProject/Files.cs
+++ androidTestProject/androidTestProject/Files.cs
@@ -138,7 +138,7 @@ public class ~Presenter implements ~Contract.Presenter {
         public const string FRAGMENTLAYOUT = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <RelativeLayout xmlns:android=""http://schemas.android.com/apk/res/android""
     xmlns:tools=""http://schemas.android.com/tools""
-    android:id=""@+id/activity_%""
+    android:id=""@+id/fragment_%""
     android:layout_width=""match_parent""
     android:layout_height=""match_parent"">
 
